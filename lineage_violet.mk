68605c9594708de3cd4f61434bb3fd48e588b7de
kibria5 <mdkibria687@gmail.com>
2023-09-16 16:10:58 +0600

    violet: Include Sony Dolby
    Signed-off-by: kibria5 <mdkibria687@gmail.com>

--- lineage_violet.mk
+++ lineage_violet.mk
@@ -10,6 +10,9 @@ $(call inherit-product, device/xiaomi/violet/device.mk)
 # Inherit some common Lineage stuff.
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
+# Sony Dolby
+$(call inherit-product, vendor/dolby/config.mk)
+
 # Device identifier. This must come after all inclusions.
 PRODUCT_NAME := lineage_violet
 PRODUCT_DEVICE := violet
